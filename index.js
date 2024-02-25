f2a8e7ed3c985861c1abe804b6f41c65857de8ce
Nick <133172211+nickmcc90@users.noreply.github.com>
2024-02-25 17:55:45 -0500

    2/25

---
+++ index.js
@@ -0,0 +1,7 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+ReactDOM.render(<h1>hello world</h1>, document.getElementById("root")); // this takes two arguments. First arg: what do I want to render. Second arg: where do I want to render it?
+// in the second argument, we access the div with the ID 'root' in the html, and stick all the react into it.
+
+console.log('yes');
